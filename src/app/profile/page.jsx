6f996c0d63edf3ec419d480360c8009cffb82e6d
afzal javed <afzaljaved59832@gmail.com>
2024-04-22 10:06:46 +0530

    sending user profile data

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,12 @@
 import ProfileSection from "@/components/Section/ProfileSection/ProfileSection";
-import { useParams } from "next/navigation"
-
+import { useSelector } from "react-redux";
 const page = () => {
     const isMobile = true;
+    const userData = useSelector((state) => state.user);
+    const post = useSelector((state) => state.posts);
     return (
         <div className="w-full h-full">
-            <ProfileSection isMobile={isMobile} />
+            <ProfileSection isMobile={isMobile} userData={userData} post={post} />
         </div>
     )
 }
